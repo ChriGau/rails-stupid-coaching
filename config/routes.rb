5dd62e25e767f09729ceebd2306b6d7ee8e6a923
ChriGau <christelle.gaudron@laposte.net>
2018-05-06 19:59:58 +0200

    new project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'score/game'
+
+  get 'score/score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root  "questions#answer"
 
